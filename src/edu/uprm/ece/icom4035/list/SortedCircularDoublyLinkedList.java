761b3247e943b54826cdd84e0c073896f108d3b2
Jesus Moreno <moreno.jesus2212@gmail.com>
2019-03-28 18:35:45 -0400

    Created Nodes and completed some methods
    Created internal Node Class, added validation method for indexes,
    created Constructor for the list and implemented size(), first(),
    last(), get(int index), clear(), contains(E e) and isEmpty().

--- src/edu/uprm/ece/icom4035/list/SortedCircularDoublyLinkedList.java
+++ src/edu/uprm/ece/icom4035/list/SortedCircularDoublyLinkedList.java
@@ -2,10 +2,17 @@ package edu.uprm.ece.icom4035.list;
 
 import java.util.Iterator;
 
-
-
 public class SortedCircularDoublyLinkedList<E extends Comparable<E>> implements SortedList<E> {
-
+	private Node<E> header;
+	private int currentSize;
+	
+	public SortedCircularDoublyLinkedList() {
+		this.header = new Node<E>();
+		header.setPrev(header);
+		header.setNext(header);
+		this.currentSize = 0;
+	}
+	
 	@Override
 	public Iterator<E> iterator() {
 		// TODO Auto-generated method stub
@@ -20,8 +27,7 @@ public class SortedCircularDoublyLinkedList<E extends Comparable<E>> implements 
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return currentSize;
 	}
 
 	@Override
@@ -44,38 +50,49 @@ public class SortedCircularDoublyLinkedList<E extends Comparable<E>> implements 
 
 	@Override
 	public E first() {
-		// TODO Auto-generated method stub
-		return null;
+		return header.getNext().getElement();
 	}
 
 	@Override
 	public E last() {
-		// TODO Auto-generated method stub
-		return null;
+		return header.getPrev().getElement();
 	}
 
 	@Override
 	public E get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		isValid(index);
+		int currIdx = 0;
+		Node<E> currNode = header.getNext();
+		while(currIdx < index) {
+			currNode = currNode.getNext();
+			currIdx++;
+		}
+		return currNode.getElement();
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
+		Node<E> curr = header.getNext();
+		while(curr != header) { // Header is also the tail
+			curr = curr.getNext();
+			curr.getPrev().clear();
+		}
+		this.currentSize = 0;
 		
 	}
 
 	@Override
 	public boolean contains(E e) {
-		// TODO Auto-generated method stub
+		Node<E> curr = header.getNext();
+		while(curr != header) {
+			if(curr.getElement().equals(e)) return true;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return currentSize == 0;
 	}
 
 	@Override
@@ -108,5 +125,44 @@ public class SortedCircularDoublyLinkedList<E extends Comparable<E>> implements 
 		return null;
 	}
 	
-
+	private void isValid(int index) throws IndexOutOfBoundsException {
+		if(index < 0 || index >= this.size()) throw new IndexOutOfBoundsException("Number " + index + " is not a valid index.");
+	}
+	
+	//////////////// INTERNAL NODE CLASS ////////////////
+	public static class Node<E extends Comparable<E>> {
+		E element;
+		Node<E> prev, next;
+		
+		Node(){
+			element = null;
+			prev = next = null;
+		}
+		
+		Node(E element){
+			this.element = element;
+			prev = next = null;
+		}
+		
+		Node(E element, Node<E> prev, Node<E> next){
+			this.element = element;
+			this.prev = prev;
+			this.next = next;
+		}
+		
+		//Helper function to clear Node Data
+		private void clear() {
+			element = null;
+			prev = next = null;
+		}
+		
+		//Getters
+		public E getElement() {return element;}
+		public Node<E> getPrev() {return prev;}
+		public Node<E> getNext() {return next;}
+		
+		//Setters
+		public void setPrev(Node<E> newPrev) {this.prev = newPrev;}
+		public void setNext(Node<E> newNext) {this.next = newNext;}
+	}
 }
