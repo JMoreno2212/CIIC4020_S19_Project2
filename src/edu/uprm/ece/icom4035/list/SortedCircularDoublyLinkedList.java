a7bd97856123fd7f4e258aba4441b2c851e78d10
Jesús Moreno <jesus.moreno@upr.edu>
2019-04-02 19:29:30 -0400

    Imported JUnit Test Cases & Finished Project!!

--- src/edu/uprm/ece/icom4035/list/SortedCircularDoublyLinkedList.java
+++ src/edu/uprm/ece/icom4035/list/SortedCircularDoublyLinkedList.java
@@ -16,13 +16,23 @@ public class SortedCircularDoublyLinkedList<E extends Comparable<E>> implements 
 
 	@Override
 	public Iterator<E> iterator() {
-		return new SortedListForwardIterator<>(this);
+		return new SortedListForwardIterator();
 	}
 
 	@Override
 	public boolean add(E obj) {
-		// TODO Auto-generated method stub
-		return false;
+		Node<E> curr = header.getNext();
+		if(currentSize > 0 && obj.compareTo(curr.getElement()) <= 0) {
+			addNodeBetween(obj, header, curr);
+		} else {
+			while(curr.getNext() != header) {
+				if(obj.compareTo(curr.getNext().getElement()) <= 0) break;
+				curr = curr.getNext();
+			}
+			addNodeBetween(obj, curr, curr.getNext());
+		}
+		currentSize++;
+		return true;
 	}
 
 	@Override
@@ -32,20 +42,49 @@ public class SortedCircularDoublyLinkedList<E extends Comparable<E>> implements 
 
 	@Override
 	public boolean remove(E obj) {
-		// TODO Auto-generated method stub
+		Node<E> curr = header.getNext();
+		while(curr != null) {
+			if(curr.getElement().equals(obj)) {
+				curr.getPrev().setNext(curr.getNext());
+				curr.getNext().setPrev(curr.getPrev());
+				currentSize--;
+				return true;
+			}
+			curr = curr.getNext();
+		}
 		return false;
 	}
 
 	@Override
 	public boolean remove(int index) {
-		// TODO Auto-generated method stub
-		return false;
+		int validIdx = isValid(index);
+		int currIdx = 0;
+		Node<E> curr = header.getNext();
+		while(currIdx < validIdx) {
+			curr = curr.getNext();
+			currIdx++;
+		}
+		curr.getPrev().setNext(curr.getNext());
+		curr.getNext().setPrev(curr.getPrev());
+		curr.clear();
+		currentSize--;
+		return true;
 	}
 
 	@Override
 	public int removeAll(E obj) {
-		// TODO Auto-generated method stub
-		return 0;
+		int removed = 0;
+		Node<E> curr = header.getNext();
+		while(curr != header) {
+			if(curr.getElement().equals(obj)) {
+				curr.getPrev().setNext(curr.getNext());
+				curr.getNext().setPrev(curr.getPrev());
+				removed++;
+			}
+			curr = curr.getNext();
+		}
+		currentSize -= removed;
+		return removed;
 	}
 
 	@Override
@@ -60,10 +99,10 @@ public class SortedCircularDoublyLinkedList<E extends Comparable<E>> implements 
 
 	@Override
 	public E get(int index) {
-		this.isValid(index);
+		int validIdx = isValid(index);
 		int currIdx = 0;
 		Node<E> currNode = header.getNext();
-		while(currIdx < index) {
+		while(currIdx < validIdx) {
 			currNode = currNode.getNext();
 			currIdx++;
 		}
@@ -77,7 +116,9 @@ public class SortedCircularDoublyLinkedList<E extends Comparable<E>> implements 
 			curr = curr.getNext();
 			curr.getPrev().clear();
 		}
-		this.currentSize = 0;
+		header.setNext(header);
+		header.setPrev(header);
+		currentSize = 0;
 
 	}
 
@@ -97,29 +138,38 @@ public class SortedCircularDoublyLinkedList<E extends Comparable<E>> implements 
 
 	@Override
 	public Iterator<E> iterator(int index) {
-		return new SortedListForwardIterator<>(this, index);
+		return new SortedListForwardIterator(index);
 	}
 
 	@Override
 	public int firstIndex(E e) {
-		// TODO Auto-generated method stub
-		return 0;
+		int idx = 0;
+		for(E element : this) {
+			if(element.equals(e)) return idx;
+			idx++;
+		}
+		return -1;
 	}
 
 	@Override
 	public int lastIndex(E e) {
-		// TODO Auto-generated method stub
-		return 0;
+		int idxToReturn = -1;
+		int idx = 0;
+		for(E element : this) {
+			if(element.equals(e)) idxToReturn = idx;
+			idx++;
+		}
+		return idxToReturn;
 	}
 
 	@Override
 	public ReverseIterator<E> reverseIterator() {
-		return new SortedListReverseIterator<>(this);
+		return new SortedListReverseIterator();
 	}
 
 	@Override
 	public ReverseIterator<E> reverseIterator(int index) {
-		return new SortedListReverseIterator<>(this, index);
+		return new SortedListReverseIterator(index);
 	}
 
 	/**
@@ -128,8 +178,37 @@ public class SortedCircularDoublyLinkedList<E extends Comparable<E>> implements 
 	 * @param index - the index that will be verified
 	 * @throws IndexOutOfBoundsException when index is not valid within the list
 	 */
-	private void isValid(int index) throws IndexOutOfBoundsException {
+	private int isValid(int index) throws IndexOutOfBoundsException {
 		if(index < 0 || index > this.size()) throw new IndexOutOfBoundsException("Number " + index + " is not a valid index.");
+		return index;
+	}
+
+	/**
+	 * This method allows you to locate a Node<E> within the list.
+	 * @param index - The index of the Node you want
+	 * @return The Node<E> at the specified index
+	 */
+	private Node<E> getNode(int index) {
+		int validIdx = isValid(index);
+		int currIdx = 0;
+		Node<E> currNode = header.getNext();
+		while(currIdx < validIdx) {
+			currNode = currNode.getNext();
+			currIdx++;
+		}
+		return currNode;
+	}
+
+	/**
+	 * This method adds a Node with the specified object in between the two specified Nodes.
+	 * @param obj - The element of the new Node to be added
+	 * @param previous - The previous Node of the new one
+	 * @param next - The following Node of the new one
+	 */
+	private void addNodeBetween(E obj, Node<E> previous, Node<E> next) {
+		Node<E> newNode = new Node<>(obj, previous, next);
+		previous.setNext(newNode);
+		next.setPrev(newNode);
 	}
 
 	///////////////////////////////// INTERNAL NODE CLASS /////////////////////////////////
@@ -177,41 +256,38 @@ public class SortedCircularDoublyLinkedList<E extends Comparable<E>> implements 
 	/**
 	 * This class will traverse through the list moving in the forward direction.
 	 */
-	@SuppressWarnings("hiding")
-	public class SortedListForwardIterator<E extends Comparable<E>> implements Iterator<E> {
-		private SortedList<E> list;
-		private int start;
-		private int current;
+
+	public class SortedListForwardIterator implements Iterator<E> {
+		private Node<E> start;
+		private Node<E> current;
 
 		/**
 		 * This iterator starts traversing the list forwardly at the beginning.
 		 */
-		public SortedListForwardIterator(SortedList<E> list) {
-			this.list = list;
-			this.start = 0;
-			this.current = 0;
+		public SortedListForwardIterator() {
+			this.start = header;
+			this.current = header.getNext();
 		}
 		/**
 		 * This iterator allows you to choose the starting index of the forward iterator.
 		 * @param index - the initial index of your iterator
 		 */
-		public SortedListForwardIterator(SortedList<E> list, int index) {
-			this.list = list;
-			this.start = index;
-			this.current = index;
+		public SortedListForwardIterator(int index) {
+			this.start = getNode(index).getPrev();
+			this.current = getNode(index);
 		}
 
 		@Override
 		public boolean hasNext() {
-			return current != ((size() + start - 1) % size());
+			if(isEmpty()) return false;
+			return current != start;
 		}
 
 		@Override
 		public E next() {
 			if(!this.hasNext()) throw new NoSuchElementException("There are no more elements");
-			E etr = list.get(current % size());
-			current = (current + 1) % size();
-			return etr;
+			current = current.getNext();
+			return current.getPrev().getElement();
 		}
 	}
 
@@ -219,41 +295,38 @@ public class SortedCircularDoublyLinkedList<E extends Comparable<E>> implements 
 	/**
 	 * This class will traverse through the list moving in the reverse direction.
 	 */
-	@SuppressWarnings("hiding")
-	public class SortedListReverseIterator<E extends Comparable<E>> implements ReverseIterator<E> {
-		private SortedList<E> list;
-		private int start;
-		private int current;
+
+	public class SortedListReverseIterator implements ReverseIterator<E> {
+		private Node<E> start;
+		private Node<E> current;
 
 		/**
-		 * This iterator starts traversing the list reversely at the beginning if it.
+		 * This iterator starts traversing the list forwardly at the beginning.
 		 */
-		public SortedListReverseIterator(SortedList<E> list) {
-			this.list = list;
-			this.start = 0;
-			this.current = 0;
+		public SortedListReverseIterator() {
+			this.start = header;
+			this.current = header.getPrev();
 		}
 		/**
-		 * This iterator allows you to choose the starting index of the reverse iterator.
+		 * This iterator allows you to choose the starting index of the forward iterator.
 		 * @param index - the initial index of your iterator
 		 */
-		public SortedListReverseIterator(SortedList<E> list, int index) {
-			this.list = list;
-			this.start = index;
-			this.current = index;
+		public SortedListReverseIterator(int index) {
+			this.start = getNode(index).getNext();
+			this.current = getNode(index);
 		}
 
 		@Override
 		public boolean hasPrevious() {
-			return current != ((size() + start + 1) % size());
+			if(isEmpty()) return false;
+			return current != start;
 		}
 
 		@Override
 		public E previous() {
 			if(!this.hasPrevious()) throw new NoSuchElementException("There are no more elements");
-			E etr = list.get(current % size());
-			current = (current - 1) % size();
-			return etr;
+			current = current.getPrev();
+			return current.getNext().getElement();
 		}
 	}
 }
